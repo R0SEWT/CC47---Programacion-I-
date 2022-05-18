6d0efa98836ba978698afb41520735f161d16523
R0SEWT <102562850+R0SEWT@users.noreply.github.com>
2022-05-18 06:28:16 -0500

    Create Ejercicio7.cpp

---
+++ Semana 9/Hoja 1/Ejercicio7.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+bool EsBiciesto(int anio) { //año 0 es biciesto
+	if (anio % 400 == 0) {
+		return true;
+	}
+	if (anio % 100 == 0) {
+		return false;
+	}
+	if (anio % 4 == 0) {
+		return true;
+	}
+	return false;
+}
+
+int  Ingresar_anio() {
+	int anio;
+	cout << "Introduzca el año a evaluar: "; cin >> anio;
+	return anio;
+}
+
+int main() {
+	int anio = Ingresar_anio();
+	cout<<EsBiciesto(anio)<<endl;
+
+	return 0;
+}
